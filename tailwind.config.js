1fa6301f19a87a6aec2f1c1ac0158a8ef8318ca7
Hernández Maikol <maikol_hernandez_rm@hotmail.com>
2022-04-18 14:02:56 -0400

    Project - tailwind - extending font and colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,16 @@
 module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{vue,js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.jsx"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: { manrope: ["Manrope"] },
+      colors: {
+        "light-cyan": "hsl(193, 38%, 86%)",
+        "neon-green": "hsl(150, 100%, 66%)",
+        "grayish-blue": "hsl(217, 19%, 38%)",
+        "grayish-blue-dark": "hsl(217, 19%, 24%)",
+        "dark-blue": "hsl(218, 23%, 16%)"
+      }
+    }
   },
-  plugins: [],
+  plugins: []
 }
